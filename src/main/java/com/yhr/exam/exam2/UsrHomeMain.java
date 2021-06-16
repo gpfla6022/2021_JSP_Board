09f7920378f4f1b50fda274f9622c080eaf30263
gpfla6022 <gpfla3503@gmail.com>
2021-06-17 03:16:00 +0900

    servlet UTF-8 setting

--- src/main/java/com/yhr/exam/exam2/UsrHomeMain.java
+++ src/main/java/com/yhr/exam/exam2/UsrHomeMain.java
@@ -18,9 +18,22 @@ public class UsrHomeMain extends HttpServlet {
     }
 
 	
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) 
+			throws ServletException, IOException {
 	
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		String name = request.getParameter("name");
+		
+		// 들어오는 파라미터를 UTF-8로 해석
+		request.setCharacterEncoding("UTF-8");
+		
+		
+		// 서블릿이 HTML 파일을 만들 때 UTF-8로 쓰기
+		response.setCharacterEncoding("UTF-8");
+		//HTML이 UTF-8형식이라는 것을 브라우저에게 알린다.
+		response.setContentType("text/html; charset=UTF-8");
+		
+		response.getWriter().append("안녕");
+		response.getWriter().append(name);
 	}
 
 	
